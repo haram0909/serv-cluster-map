3e5068f000b0ba901c21f976b91970715c850114
HRK <kimharam08@gmail.com>
2021-11-23 22:45:31 +0900

    fix! (Account, Profiles): add server-side validation for creating my profile route
    - [POST : /profiles] Added server-side validation to confirm that the account does NOT have a valid linked profile, before actually creating an profile and linking it to the account
    
    BREAKING CHANGE:
    -  /profile/new route deprecated
    - new profiles can only be created as linked profile to an account

--- index.js
+++ index.js
@@ -68,9 +68,6 @@ app.get('/profiles', async (req, res) => {
 
 
 
-
-
-
 app.get('/profiles/:id', async (req, res) => {
     const profile = await Profile.findById(req.params.id).populate('account');
     res.render('profiles/show.ejs', { profile });
@@ -88,20 +85,16 @@ app.patch('/profiles/:id', async (req, res) => {
     //console.log(`req.body = ${JSON.stringify(req.body)}`);
 
     const { id } = req.params;
-    //build up an obj with properties to update, based on form data
-    //will have to handle other referencing properties...
     const updateProfile = {
         introduction: req.body.profile.introduction,
         location: req.body.profile.location,
-
         //should be an array of objects
         //should flash messages for items that did not meet criteria or send back
-        //instead of filtering on behald in the back without explanation
+        //instead of filtering out in the back without explanation
         skills: req.body.profile.skills.filter(obj => (obj.proglang !== "" && obj.experience !== "" && obj.experience >= 0)),
 
         //should be boolean
         availability: req.body.profile.availability,
-        //req.body.availability,
 
         //should be an array of objects
         //should flash messages for items that did not meet criteria or send back
@@ -128,7 +121,6 @@ app.patch('/profiles/:id', async (req, res) => {
 
 
     res.redirect(`/profiles/${profile._id}`);
-
 });
 
 
@@ -136,21 +128,12 @@ app.delete('/profiles/:id', async (req, res) => {
     const { id } = req.params;
     const profileToDelete = await Profile.findById(id);
     await Review.deleteMany({ _id: { $in: profileToDelete.reviews } });
-
-    console.log(`Profile to be deleted = ${JSON.stringify(profileToDelete)}`);
-
-    //no need to delete objectid from Account, as it is only 1 field
-    //& the logic to allow for profile creation could be 
-    // checking whether the objectid stored(if exists on the account.profile) exists from the profiles collection
-    //if it does NOT, then can create an object, at the time, the account's profile's objectid will be updated
-
-    //Account will NOT be auto deleted, when profile gets deleted.
-    //await Review.deleteMany({_id: {$in: doc.reviews} })
+    // console.log(`Profile to be deleted = ${JSON.stringify(profileToDelete)}`);
     await Profile.findByIdAndDelete(id);
+    // console.log(`Account id to delete this profile from = ${req.body.accountId}`);
 
-    console.log(`Account id to delete this profile from = ${req.body.accountId}`);
-    await Account.findByIdAndUpdate(req.body.accountId, {profile: null});
-
+    //set the profile linked account's profile property to null -> allow the account to create new profile
+    await Account.findByIdAndUpdate(req.body.accountId, { profile: null });
     res.redirect('/profiles');
 })
 
@@ -168,50 +151,44 @@ app.post('/account/login', async (req, res) => {
     res.redirect('/profiles');
 })
 
-
 app.get('/account/register', async (req, res) => {
     res.render('accounts/register.ejs');
 })
 
 app.post('/account/register', async (req, res) => {
-
-    console.log(req.body);
-    console.log(`new account = ${JSON.stringify(req.body.account)}`);
-
-    //build up account obj 
+    // console.log(req.body);
+    // console.log(`new account = ${JSON.stringify(req.body.account)}`);
     const account = new Account(req.body.account);
-
     await account.save();
     res.redirect(`/account/${account._id}`);
 });
 
 
 //my account info - first name, last name, email account
-    // will eventually have to update navbar to enable this
+// will eventually have to update navbar to enable this
 app.get('/account/:id', async (req, res) => {
     const account = await Account.findById(req.params.id).populate('profile');
 
-    //an account mayonly have 1 profile = this will check whether the account has a valid profile
+    //an account may only have 1 profile = this will check whether the account has a valid profile
     //if not, the account will be able to create a new profile
     //if does, the account will be able to navigate to the profile & edit 
     const haveProfile = account.profile ? true : await Profile.findById(account.profile);
+    // console.log(`Have a profile = ${haveProfile}`);
 
-    console.log(`Have a profile = ${haveProfile}`);
     res.render('accounts/show.ejs', { account, haveProfile });
 })
 
 //edit account - first name, last name, email account
-app.get('/account/:id/edit', async (req,res)=>{
+app.get('/account/:id/edit', async (req, res) => {
     const account = await Account.findById(req.params.id);
     const haveProfile = account.profile ? true : await Profile.findById(account.profile);
-
-    console.log(`Have a profile = ${haveProfile}`);
+    // console.log(`Have a profile = ${haveProfile}`);
 
     res.render('accounts/edit.ejs', { account, haveProfile });
 })
 
 //updates account info
-app.patch('/account/:id', async(req,res)=>{
+app.patch('/account/:id', async (req, res) => {
     // console.log(`req.body = ${JSON.stringify(req.body)}`);
     const { id } = req.params;
     const updateProfile = {
@@ -228,17 +205,13 @@ app.patch('/account/:id', async(req,res)=>{
 //$$$$$$!!!!!!! All profile routes need authorization check before any edit ability
 
 
-//should ONLY allow creation of profile through this route... 
-//eventually will have to disable the profiles/new route & only allow route through here 
-//AND have 2 way object id updating, --> because 1 account = 1 or 0 profile
-
 //should only be allowed when current account does NOT have profile
 app.get('/account/:id/profile/new', async (req, res) => {
     const accountId = req.params.id;
     console.log(JSON.stringify(res.body));
 
     //have the logic check the current account does NOT have a valid profile
-    res.render('profiles/new.ejs', {accountId});
+    res.render('profiles/new.ejs', { accountId });
 });
 
 //!!!! will have to establish 2way referencing of profile object id and account object id
@@ -249,10 +222,11 @@ app.get('/account/:id/profile/new', async (req, res) => {
 app.post('/profiles', async (req, res) => {
     //the created profile will have to be added to the account
     // const profile = await Profile.findById(req.params.id).populate('account');
-    console.log(req.body);
+    // console.log(req.body);
+
     //set separated availability path and add to profile
     //req.body.profile.availability = req.body.availability;
-    console.log(`new profile = ${JSON.stringify(req.body.profile)}`);
+    // console.log(`new profile = ${JSON.stringify(req.body.profile)}`);
 
 
 
@@ -260,49 +234,36 @@ app.post('/profiles', async (req, res) => {
     req.body.profile.offerings = req.body.profile.offerings.filter(obj => (obj.service !== "" && obj.price !== "" && obj.price >= 0));
     console.log(`cleaned new profile = ${JSON.stringify(req.body.profile)}`);
 
+    const account = await Account.findById(req.body.accountId);
 
-    //build up profile obj just like edit route, and also have filterings
+    //confirm that the account does NOT have a valid linked profile
+    const haveProfile = account.profile ? true : await Profile.findById(account.profile);
+    if (haveProfile) {
+        throw new Error("This Account already has a valid My Profile. An account is not allowed to have more than 1 valid profile.");
+    }
+
+
     const profile = new Profile(req.body.profile);
     //need current session's account 
     //!!!!!!!curr account.profile = profile._id; ** just like seeding
-    const account = await Account.findById(req.body.accountId);
+
     profile.account = account;
     await profile.save();
     await console.log('saved profile');
 
     const updatedProfile = await Account.findByIdAndUpdate(req.body.accountId, { profile: profile }, { upsert: true });
-    console.log(`updated profile = ${JSON.stringify(updatedProfile)}`);
-
+    // console.log(`updated profile = ${JSON.stringify(updatedProfile)}`);
 
     //https://stackoverflow.com/questions/38011068/how-to-remove-object-taking-into-account-references-in-mongoose-node-js
-    // await account.profile.remove(
-    //     {"_id": {"$in": account.profile}}
-    // )
-    // //so... object id is IMMUTABLE.... --> need another way... such as email? 
-    //     //---------> so it should be... array of object ids.. so we could pop entire obj..
-    //     // or... can we remove the property? 
-    // await Account.findByIdAndUpdate(req.body.accountId, { $set: profile }, { upsert: true })
-    // // const obj = {profile: newProfileId};
-    // // const updatedProfile = await Account.findByIdAndUpdate(id, { $set:  obj }, { upsert: true });
-
-    // await console.log(`Updated profile's id = ${profile._id}`);
-    // await console.log(`Updated profile's account id = ${profile.account}`);
-    // await console.log(`Updated account's id = ${account._id}`);
-    // await console.log(`Updated account's profile id = ${account.profile}`);
-
-    //    &&&& there is no 2way referencing established before redirection = breaks ejs
     res.redirect(`/profiles/${profile._id}`);
 });
 
-app.delete('/account/:id', async (req,res)=>{
+app.delete('/account/:id', async (req, res) => {
 
-        // const accountToDelete = await Account.findById(req.params.id);
-        // const profileToDelete = await Profile.findById(accountToDelete._id);
-        const profileToDelete = await Profile.findById(req.body.profileId);
-        await Review.deleteMany({ _id: { $in: profileToDelete.reviews } });
-        // await Profile.findByIdAndDelete(accountToDelete._id);
-        await Profile.findByIdAndDelete(req.body.profileId);
-        await Account.findByIdAndDelete(req.params.id);
+    const profileToDelete = await Profile.findById(req.body.profileId);
+    await Review.deleteMany({ _id: { $in: profileToDelete.reviews } });
+    await Profile.findByIdAndDelete(req.body.profileId);
+    await Account.findByIdAndDelete(req.params.id);
 
     res.redirect(`/profiles`);
 
