59b65dd526f5ab43e083e00e427dfd3e9a895fb6
HRK <kimharam08@gmail.com>
2021-11-28 01:16:43 +0900

    refactor(boilerplate): remove client-side form validation script from boilerplate to separate file
    Remove client-side form validation javascript from boilerplate.ejs to separate javascript file that will be served as static asset

---
+++ public/javascripts/validateForms.js
@@ -0,0 +1,31 @@
+//https://stackoverflow.com/questions/56075450/how-to-disable-bootstrap-4-validation-style-from-valid-controls
+    //bootstrap form validation to run only on forms with .needs-validation
+    //for inputs under div of .form-group and .validate-me
+    //if input is not under .form-group and .validate-me, they will not be subject to client-side validation
+    //thus, will NOT show any visual validation feedback upon 'submit'
+
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+
+    // Get all form-groups that needs to be validated
+    let validateGroup = document.getElementsByClassName('validate-me');
+
+    // Loop over them and prevent submission
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                // Added validation class to all form-groups in need of validation
+                for (let i = 0; i < validateGroup.length; i++) {
+                    validateGroup[i].classList.add('was-validated');
+                }
+            }, false);
+        });
+})();
