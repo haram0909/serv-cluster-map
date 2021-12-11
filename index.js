4b32d5f13b55848696666db677565be54d35121f
HRK <kimharam08@gmail.com>
2021-12-11 18:59:54 +0900

    misc: misc comments

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ app.delete('/profiles/:id', isLoggedIn, catchAsync(async (req, res) => {
 
 
     if (profileToDelete.reviews.length > 0) {
-        console.log('Deleting linked reviews');
+        console.log('Deleting reviews on this profile');
         await Review.deleteMany({ _id: { $in: profileToDelete.reviews } });
         req.flash('success', 'Successfully deleted reviews on the profile.');
     } else {
@@ -636,6 +636,52 @@ app.delete('/profiles/:id/review', isLoggedIn, catchAsync(async (req, res) => {
     req.flash('success', 'Successfully deleted reviews on the profile.');
     res.redirect(`/profiles/${req.params.id}`);
 }));
+
+//need edit review route - patch? 
+//--> needs form and view for review edit... will skip Edit for now... 
+//need to check if logged in 
+//need to check if have authority to edit
+//find the review from reviews array and update
+//flash msg & redirect 
+// app.patch('/profiles/:id/review', isLoggedIn, validateReview, catchAsync(async (req, res)=> {
+//         // console.log(`skills = ${req.body.profile.skills.filter(obj => (obj.proglang !== "" && obj.experience !== "" && obj.experience >= 0))}`);
+//         // console.log(`offerings = ${req.body.profile.offerings.filter(obj => (obj.service !== "" && obj.price !== "" && obj.price >= 0))}`);
+//         //console.log(`req.body = ${JSON.stringify(req.body)}`);
+
+//         const { id } = req.params;
+//         console.log('req.body = ');
+//         console.log(req.body);
+//         console.log('res.locals.profile = ');
+//         console.log(JSON.stringify(res.locals.profile));
+
+//         const profile = await Profile.findByIdAndUpdate(id, { $set: res.locals.profile }, { upsert: true, new: true });
+//         console.log(`Updated profile = ${profile}`);
+//         //need current session's account 
+//         //curr account.profile = profile._id; ** just like seeding
+
+//         // const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+//         // campground.images.push(...imgs);
+//         // await campground.save();
+//         // if (req.body.deleteImages) {
+//         //     for (let filename of req.body.deleteImages) {
+//         //         await cloudinary.uploader.destroy(filename);
+//         //     }
+//         //     await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+//         // }
+//         if (!profile) {
+//             req.flash('error', 'Failed to update! Cannot find that profile!');
+//             return res.redirect('/profiles');
+//         }
+
+//         req.flash('success', 'Successfully updated my profile!')
+//         res.redirect(`/profiles/${profile._id}`);
+//     }));
+
+
+
+
+
+//routes for account
 app.get('/account/login', async (req, res) => {
 
     //check if the user is already logged in
