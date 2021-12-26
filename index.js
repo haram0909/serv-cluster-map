e8be43fad0e567b55ccab0a861a59de34cc18732
HRK <kimharam08@gmail.com>
2021-12-26 13:01:09 +0900

    feat(security): add helmet and configure content security policy
    - Enabled all middlewares of helmet for basic security
    - Configured content security policy exception for helmet

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const LocalStrategy = require('passport-local');
 
 //basic security packages
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+
+
 //dev dependencies
 const morgan = require('morgan');
 
@@ -130,6 +133,54 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+//security - use helmet
+    //enable all middlewares of helmet for security
+app.use(helmet());
+    //helmet - content security policy exception configuration
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/servclustermap/", 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 //passport configuration
 //doc = http://www.passportjs.org/docs/configure/
