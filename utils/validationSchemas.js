75ea13b608d1d48e66b165bf80e258ecea3e1b65
HRK <kimharam08@gmail.com>
2021-12-26 12:59:22 +0900

    feat(security): add sanitize-html and extend joi validation
    Added sanitize-html to extend joi server-side validation to escape html tags from string input data

--- utils/validationSchemas.js
+++ utils/validationSchemas.js
@@ -1,22 +1,46 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+//basic security 
+const sanitizeHtml = require('sanitize-html');
 
+//attaching method to joi to clean strings that gets validated by joi to be sanitized by sanitize-html
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} is NOT allowed to include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 
 module.exports.joiProfileSchema = Joi.object({
     profile: Joi.object({
-        introduction: Joi.string(),
+        introduction: Joi.string().escapeHTML(),
         //images should be array of objects?
         //images: Joi.optional(),
-        location: Joi.string().required(),
+        location: Joi.string().required().escapeHTML(),
         geometry: Joi.object().optional(),
         skills: Joi.array().items(Joi.object({
-            proglang: Joi.string(),
+            proglang: Joi.string().escapeHTML(),
             experience: Joi.number().min(0).max(120)
             //valid if either have both proglang AND experience or have neither
         }).and('proglang', 'experience'), Joi.object().strip()),
         availability: Joi.boolean().required(),
         offerings: Joi.array().items(Joi.object({
-            service: Joi.string(),
+            service: Joi.string().escapeHTML(),
             price: Joi.number().min(0).max(10000)
             //valid if either have both service AND price or have neither
         }).and('service', 'price'), Joi.object().strip())
@@ -27,9 +51,9 @@ module.exports.joiProfileSchema = Joi.object({
 
 module.exports.joiAccountUpdateSchema = Joi.object({
     account: Joi.object({
-        firstname: Joi.string().required(),
-        lastname: Joi.string().required(),
-        email: Joi.string().email({ tlds: false }).required(),
+        firstname: Joi.string().required().escapeHTML(),
+        lastname: Joi.string().required().escapeHTML(),
+        email: Joi.string().email({ tlds: false }).required().escapeHTML(),
         
     }).required()
 });
@@ -37,18 +61,18 @@ module.exports.joiAccountUpdateSchema = Joi.object({
 
 module.exports.joiAccountRegisterSchema = Joi.object({
     account: Joi.object({
-        firstname: Joi.string().required(),
-        lastname: Joi.string().required(),
-        email: Joi.string().email({ tlds: false }).required(),
-        username: Joi.string().required(),
-        password: Joi.string()./*.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))*/required()
+        firstname: Joi.string().required().escapeHTML(),
+        lastname: Joi.string().required().escapeHTML(),
+        email: Joi.string().email({ tlds: false }).required().escapeHTML(),
+        username: Joi.string().required().escapeHTML(),
+        password: Joi.string()./*.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))*/required().escapeHTML()
     }).required()/*.with('password','confirm_password')*/
 });
 
 
 module.exports.joiReviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().min(0).max(5).required()
     }).required()
 });
