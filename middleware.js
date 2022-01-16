b55a7725f394a1f4c3f99b2fc7d634bd70116796
HRK <kimharam08@gmail.com>
2022-01-16 15:45:45 +0900

    chore(middleware): clean up dev comments in middleware.js

--- middleware.js
+++ middleware.js
@@ -71,15 +71,9 @@ module.exports.validateProfileSearch = (req, res, next) => {
     const validationResult = joiSearchProfileSchema.validate(req.body);
 
     if (validationResult.error) {
-        console.log(validationResult.error)
-        
         req.flash('error', 'Invalid keyword(s) detected! Please enter valid keyword(s)!');
         return res.redirect(`/search/experts`);
-
     } else {
-        console.log()
-        console.log(validationResult)
-        console.log()
         res.locals.searchProfile = validationResult.value;
         next();
     }
@@ -89,8 +83,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     //updated to use .isAuthenticated() method from passport
     //instead of checking for res.locals.currentAccount exists
     if (!req.isAuthenticated()) {
-
-        //save the original url that was requested
+        //save the original url that was requested for redirecting back to originally requested page, after logging in
         req.session.returnTo = req.originalUrl
 
         const errMsg = 'Needs to be logged in first!'
@@ -98,18 +91,12 @@ module.exports.isLoggedIn = (req, res, next) => {
         req.flash('error', errMsg);
         return res.redirect('/account/login')
     } else {
-        // console.log('Logged in account exists');
-        //for potential redirection, if total file upload size is larger than 10MB limit
-        // console.log('At renderCreation profile form -->')
-        // console.log(req.params.id)
-        // req.session.fromNewProfileCreation = req.params.id
         next();
     }
 }
 
 
 module.exports.isAccountOwner = (req, res, next) => {
-
     if (!res.locals.currentAccount._id.equals(req.params.id)) {
         req.flash('error', 'Cannot interact with accounts that is not yours!');
         return res.redirect(`/profiles`);
@@ -120,11 +107,9 @@ module.exports.isAccountOwner = (req, res, next) => {
 }
 
 module.exports.isProfileOwner = (req, res, next) => {
-
     //check whether the currentAccount has a profile AND ALSO is the owner of this profile
     if (res.locals.currentAccount.profile && req.params.id === res.locals.currentAccount.profile.toString()) {
         // console.log('IS the profile owner');
-        // console.log();
         next();
     } else {
         // console.log('NOT the profile owner');
@@ -137,29 +122,24 @@ module.exports.isProfileOwner = (req, res, next) => {
     //if truthy, next() to allow uploading
     //if falsy, redirect to prevent uploading
 module.exports.fileSizeIsBelowLimit = async (req, res, next) => {
-    // console.log()
     // console.dir(req.headers['content-length'])
+    //set file size limit for uploading at once to 10MB
     const fileSizeLimit = 10485760; // 10 Mb //5242880; //10MB  
-    // console.log()
 
-    //profile edit form should NOT even show upload form, 
-    //if the file size in total is larger than 10MB. 
-    //else, next()
-
+    //if the file size in total is larger than 10MB 
     if (req.headers['content-length'] && fileSizeLimit < parseInt(req.headers['content-length'])) {
         // console.log(`File(s) to upload is larger than 10MB`);
-
         req.flash('error', 'Cannot upload more than 10MB of images at once!');
         
-        //might face file size limit issue when
-            //because using callback function, this middleware might not need to be async/await
+        //might face file size limit issue when ...
+                //because using callback function, this middleware might not need to be async/await
         await Profile.countDocuments({_id: req.params.id}, function(err, count){
-            //at editing profile or
+            //...at editing profile or
             if(count > 0){
                 // console.log(count)
                 console.log('Profile with the id exists, thus Was trying to edit profile')
                 return res.redirect(`/profiles/${req.params.id}/edit`);
-            //at creating new profile 
+            //...at creating new profile 
             }else{
                 // console.log(count)
                 console.log('Profile does not exists, thus redirected to create new profile page')
