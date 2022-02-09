312add5b2345f7b5b9f0bf3653cccec94e29a1b3
HRK <kimharam08@gmail.com>
2022-02-09 23:53:42 +0900

    chore(profile): clean up comment outs from profile route

--- routes/profiles.js
+++ routes/profiles.js
@@ -20,17 +20,11 @@ const upload = multer({ storage });
 //express-pagination
 const paginate = require('express-paginate');
 
-
 //middleware
 const { validateProfile, 
     isLoggedIn, 
     isProfileOwner, 
     fileSizeIsBelowLimit} = require('../middleware.js');
-
-//models
-// const Profile = require('../models/profile.js');
-// const Account = require('../models/account.js');
-// const Review = require('../models/review.js');
 
 //controllers
 const profilesController = require('../controllers/profiles.js');
@@ -43,73 +37,16 @@ const profilesController = require('../controllers/profiles.js');
 
 //routes for profiles
 router.route('/')
-    //might want pagination here, instead of full load all....
     .get(paginate.middleware(20, 100), catchAsync(profilesController.showIndex))
     //POST '/profiles' route = ONLY ACCESSIBLE THROUGH GET '/account/:id/profile/new' route  
         //with multer and cloudinary, mutler-cloudinary-storage pkgs, now uploads to cloudinary storage and store filename path(url) to mongoDB
-            //total file upload size limits of 10MB for server-side validation 
+            //total file upload size limits per upload set to 10MB through server-side validation
     .post(isLoggedIn, fileSizeIsBelowLimit, upload.array('images'), validateProfile, catchAsync(profilesController.createProfile));
-
-    //!!!using npm multer to parse form enctype multipart/form-data     
-        //upload.single() = upload single file
-            //will give req.file = contains info about the file attempted to upload 
-            //{
-            // fieldname: 'images', (because the form's field name for image upload was 'images')
-            // originalname: 'actualFileName.png',
-            // encoding: '7bit',
-            // mimetype: 'image/png',
-            // destination: 'uploads/',  (because we set multer's upload destination to 'uploads/' folder in this route file --> will eventually be cloudinary)
-            // filename: 'some unique encoded string',
-            // path: 'uploads/some encoded string from above field 'filename'', (because we set multer's upload destination to 'uploads/' folder in this route file)
-            // size: number
-            //    }
-    // .post(upload.single('images'), (req,res)=>{
-
-        //upload.array() = uploads potentially multiple files under the fieldname of 'images'
-            //will give req.file = contains info about the file attempted to upload 
-            //[
-            //    {
-            //      fieldname: 'images', (because the form's field name for image upload was 'images')
-            //      originalname: 'actualFileName.png',
-            //      encoding: '7bit',
-            //      mimetype: 'image/png',
-            //      destination: 'uploads/',  (because we set multer's upload destination to 'uploads/' folder in this route file --> will eventually be cloudinary)
-            //      filename: 'some unique encoded string',
-            //      path: 'uploads/some encoded string from above field 'filename'', (because we set multer's upload destination to 'uploads/' folder in this route file)
-            //      size: number
-            //    }
-            //]
-    // .post(upload.array('images'), (req,res)=>{
-        
-    //multer configured to upload to cloudinary storage 
-        //upload.array() = uploads potentially multiple files under the fieldname of 'images'
-            //will give req.file = contains info about the file attempted to upload 
-            //[
-            //    {
-            //      fieldname: 'images', (because the form's field name for image upload was 'images')
-            //      originalname: 'actualFileName.png',
-            //      encoding: '7bit',
-            //      mimetype: 'image/png',
-            //      path: 'cloudinary url for the file', (ex. 'https://res.cloudinary.com/servclustermap/image/upload/{string}/{filename string}{.file extention}') (because we now setted multer's upload destination to be cloudinary storage in this route file)
-            //      size: number,
-            //      filename: 'ServClusterMap/encoded unique file name string' = ({cloudinary storage name}/{filename})
-            //    }
-            //]
-    // .post(upload.array('images'), (req,res)=>{
-    //     console.log();
-    //     console.log('req.body = ');
-    //     console.log(req.body);
-    //     console.log();
-    //     console.log('req.files = ');
-    //     console.log(req.files);
-    //     console.log();
-    //     res.send(req.body);
-    // });
 
 router.route('/:id')
     .get(catchAsync(profilesController.showDetail))
      //with multer and cloudinary, mutler-cloudinary-storage pkgs, now uploads to cloudinary storage and store filename path(url) to mongoDB
-        //total file upload size limits of 10MB for server-side validation 
+         //total file upload size limits per upload set to 10MB through server-side validation
     .patch(isLoggedIn, isProfileOwner, fileSizeIsBelowLimit, upload.array('images'), validateProfile, catchAsync(profilesController.updateProfile))
     .delete(isLoggedIn, isProfileOwner, catchAsync(profilesController.destroyProfile));
 
